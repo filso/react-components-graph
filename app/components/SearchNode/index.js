34d7b387bb660bbbc8aadcc8cd629841d8bd46cf
Filip Sobczak <filsob@gmail.com>
2017-11-01 15:22:23 +0000

    htmlFor

--- app/components/SearchNode/index.js
+++ app/components/SearchNode/index.js
@@ -5,13 +5,12 @@ export default class SearchNode extends React.Component { // eslint-disable-line
   render() {
     return (
       <div className="search">
-        <label for="search-input">Search:</label>
+        <label htmlFor="search-input">Search:</label>
         <input id="search-input" type="text"></input>
       </div>
     );
   }
 }
-
 
 SearchNode.propTypes = {
 };
